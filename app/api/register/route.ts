a3992d515d1abeb3eba158f5c9552c3f684470b4
yudy <yudymayorga.yaml@gmail.com>
2026-06-11 20:16:46 -0500

    corrección correo 14

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -5,21 +5,21 @@ console.log("API KEY:", process.env.RESEND_API_KEY);
 
 const resend = new Resend(process.env.RESEND_API_KEY!);
 
-const GOOGLE_SCRIPT_URL =
-  process.env.GOOGLE_SHEETS_WEBHOOK_URL!;
+const GOOGLE_SCRIPT_URL = process.env.GOOGLE_SHEETS_WEBHOOK_URL!;
 
 export async function POST(request: Request) {
   try {
     const data = await request.json();
-    
-console.log("==================================");
-console.log("Nombre:", data.name);
-console.log("Código:", data.uniqueCode);
-console.log("QR:", data.qrImage);
-console.log("Longitud QR:", data.qrImage?.length);
-console.log("==================================");
 
-    // Guardar únicamente en Google Sheets
+    console.log("==================================");
+    console.log("Nombre:", data.name);
+    console.log("Email:", data.email);
+    console.log("Código:", data.uniqueCode);
+    console.log("QR:", data.qrImage);
+    console.log("Longitud QR:", data.qrImage?.length);
+    console.log("==================================");
+
+    // Guardar en Google Sheets
     await fetch(GOOGLE_SCRIPT_URL, {
       method: "POST",
       headers: {
@@ -28,8 +28,8 @@ console.log("==================================");
       body: JSON.stringify(data),
     });
 
-    // Enviar correo con Resend
-    await resend.emails.send({
+    // Enviar correo
+    const email = await resend.emails.send({
       from: "Día de Milagros <eventos@diademilagros.com>",
       to: data.email,
       subject: "Confirmación de inscripción - DÍA DE MILAGROS",
@@ -84,6 +84,7 @@ console.log("==================================");
       <img
       src="${data.qrImage}"
       width="240"
+      alt="Código QR"
       style="display:block;margin:auto;">
 
       </div>
@@ -96,8 +97,7 @@ console.log("==================================");
 
       <hr>
 
-      <p
-      style="text-align:center;color:#777;">
+      <p style="text-align:center;color:#777;">
       Ministerio Barcelona
       </p>
 
@@ -115,10 +115,16 @@ console.log("==================================");
       `,
     });
 
+    console.log("========== RESPUESTA RESEND ==========");
+    console.log(email);
+    console.log("======================================");
+
     return NextResponse.json({
       status: "success",
+      email,
     });
   } catch (error) {
+    console.error("ERROR API REGISTER:");
     console.error(error);
 
     return NextResponse.json(
