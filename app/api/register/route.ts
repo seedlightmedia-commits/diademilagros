311e08d824942b5c83e26517be1dc5832bb44d5a
yudy <yudymayorga.yaml@gmail.com>
2026-06-19 01:16:40 -0500

    direcciones correo2

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -76,6 +76,9 @@ export async function POST(request: Request) {
 
       <p>
       Tu inscripción ha sido confirmada correctamente.
+      <p>
+      
+      <p>
       Dirección: Cine Aribau Calle Aribau 8, Barcelona 
       Metro L1, L2 Universitat
       </p>
